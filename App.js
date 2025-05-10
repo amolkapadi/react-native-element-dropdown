ee76fdce3af239f64852a476141f31e1e15e542d
AmolSpringuplabs <amol.kapadi@springuplabs.com>
2025-05-10 11:20:48 +0530

    simple dropdown

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
+import { StatusBar, SafeAreaView, Text } from 'react-native';
+import DropdownSelect from './component/DropdownSelect';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="red"
+      />
+
+
+      <DropdownSelect />
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ component/DropdownSelect.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import { Dropdown } from 'react-native-element-dropdown';
+
+const DropdownSelect = () => {
+    const [selectedValue, setSelectedValue] = useState(null);
+
+    // Sample data for the dropdown
+    const data = [
+        { label: 'JavaScript', value: '1' },
+        { label: 'React Js', value: '2' },
+        { label: 'Node Js', value: '3' },
+        { label: 'HTML', value: '4' },
+        { label: 'CSS', value: '5' },
+    ];
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Select Subject</Text>
+            <Dropdown
+                style={styles.dropdown}
+                data={data}
+                labelField="label"
+                valueField="value"
+                placeholder="Select an option"
+                value={selectedValue}
+                onChange={item => {
+                    setSelectedValue(item.value);
+                }}
+            />
+
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 10
+    },
+
+    dropdown: {
+        height: 50,
+        borderColor: 'gray',
+        borderWidth: 1,
+        borderRadius: 8,
+        paddingHorizontal: 8,
+    },
+});
+
+export default DropdownSelect;
